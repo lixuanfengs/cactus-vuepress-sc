446dd68e5cbbb407a9e9e90e25c1278a10b72fb1
仙人球 <1183895890@qq.com>
2022-04-29 15:49:08 +0800

    修改部署文件

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ cd docs/.vuepress/dist
 # deploy to github pages
 echo 'cactuslixf.tk' > CNAME
 
+
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:lixuanfengs/cactus-vuepress.git
